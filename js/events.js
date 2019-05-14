8556701019cba63e307b1721f8de36151cb2da05
Lucas Klefasz Ferreira <ferreirak.lucas@gmail.com>
2019-05-14 02:08:34 +0000

    Automatically backed up by Learn

--- js/events.js
+++ js/events.js
@@ -1,7 +1,11 @@
-//define functions here
+function getIt() {
+  $('img').on('click', function(){
+    alert('Hey!')
+});
+}
 
 $(document).ready(function(){
 
-// call functions here
+  getIt()
 
 });
